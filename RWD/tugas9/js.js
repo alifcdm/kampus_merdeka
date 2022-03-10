4aeea46a5d5753a6d7ef9db471b79397f6758958
GeezCre <alifchandra001@gmail.com>
2022-03-10 20:34:25 +0700

    tugas rwd1

---
+++ RWD/tugas9/js.js
@@ -0,0 +1,8 @@
+$('document').ready(function () {
+  $('#changebg1').click(function () {
+    $('body').css('backgroundColor', 'white');
+  });
+  $('#changebg2').click(function () {
+    $('body').css('backgroundColor', 'black');
+  });
+});
